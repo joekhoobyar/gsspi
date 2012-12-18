b514e9186f87d9360435c6d5216738960a0ef1ee
Joe Khoobyar <jkhoobyar@tnsi.com>
2012-12-18 18:13:44 -0500

    next development lifecycle

--- src/test/java/name/khoobyar/joe/gsspi/GsspiUtilsTest.java
+++ src/test/java/name/khoobyar/joe/gsspi/GsspiUtilsTest.java
@@ -8,16 +8,16 @@ public class GsspiUtilsTest {
 
 	@Test
 	public void testGetServicePrincipalName () {
-		String spn = GsspiUtils.getServicePrincipalName ("host", "rst-etldev1");
-		assertFalse ("host/rst-etldev1".equalsIgnoreCase (spn));
-		String qhn = GsspiUtils.getQualifiedHostName ("rst-etldev1");
+		String spn = GsspiUtils.getServicePrincipalName ("host", "localhost");
+		assertFalse ("host/localhost".equalsIgnoreCase (spn));
+		String qhn = GsspiUtils.getQualifiedHostName ("localhost");
 		assertEquals ("host/"+qhn, spn);
 	}
 
 	@Test
 	public void testGetQualifiedHostName () {
-		String qhn = GsspiUtils.getQualifiedHostName ("rst-etldev1");
-		assertFalse ("rst-etldev1".equalsIgnoreCase (qhn));
+		String qhn = GsspiUtils.getQualifiedHostName ("localhost");
+		assertFalse ("localhost".equalsIgnoreCase (qhn));
 	}
 
 }
