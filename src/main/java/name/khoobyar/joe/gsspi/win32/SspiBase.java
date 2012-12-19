a1f1406f610ff8ae49022bbe10c06817ba6a40ca
Joe Khoobyar <jkhoobyar@tnsi.com>
2012-12-18 21:24:34 -0500

    build fixes...

--- src/main/java/name/khoobyar/joe/gsspi/win32/SspiBase.java
+++ src/main/java/name/khoobyar/joe/gsspi/win32/SspiBase.java
@@ -1,7 +1,20 @@
 package name.khoobyar.joe.gsspi.win32;
 
+import java.lang.reflect.Method;
+
 public class SspiBase {
-	static { NarSystem.loadLibrary(); }
+	static {
+		Class<?> narSystem;
+		try { narSystem = Class.forName("name.khoobyar.joe.gsspi.NarSystem"); }
+		catch (ClassNotFoundException e) { throw new RuntimeException (e); }
+		
+		Method loadLibrary;
+		try { loadLibrary = narSystem.getMethod ("loadLibrary"); }
+		catch (Exception e) { throw new RuntimeException (e); }
+		
+		try { loadLibrary.invoke (null); }
+		catch (Exception e) { throw new RuntimeException (e); }
+	}
 	 
 	public native void init ();
 }
