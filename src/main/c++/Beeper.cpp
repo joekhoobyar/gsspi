d0ed13063e8bc84a2d789d96ba15e2404bec9541
Joe Khoobyar <jkhoobyar@tnsi.com>
2012-12-18 20:18:43 -0500

    working on actual implementing functions now

--- src/main/c++/Beeper.cpp
+++
@@ -1,7 +0,0 @@
-#include "name_khoobyar_joe_gsspi_Beeper.h"
-#include "Windows.h"
-
-JNIEXPORT void JNICALL Java_name_khoobyar_joe_gsspi_Beeper_beep (JNIEnv *, jobject) {
-      MessageBeep(0);
-      MessageBox(NULL, "Beep", "Beeper", MB_OK);
-}
---
+++ src/main/c++/misc.cpp
@@ -0,0 +1,18 @@
+#include "name_khoobyar_joe_gsspi_Beeper.h"
+#include "Windows.h"
+
+JNIEXPORT jboolean JNICALL Java_name_khoobyar_joe_gsspi_win32_SecurityPackages(JNIEnv *env, jobject self) {
+	SECURITY_STATUS status;
+	ULONG oLength;
+	PSecPkgInfo oPackages;
+
+	status = EnumerateSecurityPackages (&oLength, &oPackages);
+	if (status != SEC_E_OK)
+		return JNI_FALSE;
+	SetLongField (env, self, (jlong) oLength);
+	FreeContextBuffer (oPackages);
+	return JNI_TRUE;
+}
+
+//MessageBeep(0);
+//MessageBox(NULL, "Beep", "Beeper", MB_OK);
